58f0da06a7d477471f3c339759200506ab159848
MajidRaimi <imajid2000z1@gmil.com>
2021-08-03 22:07:05 +0300

    Day One

---
+++ basicGUI.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+public class basicGUI {
+	public static void main(String args[]) {
+		// Take Input 
+		String name = JOptionPane.showInputDialog("Please Enter Your Name : ") ; 
+		// Show Message
+		JOptionPane.showMessageDialog(null, "Hello " + name);
+		
+		// Change String To Int 
+		int age =  Integer.parseInt(JOptionPane.showInputDialog("Please Enter Your Age : ")) ; 
+		
+		JOptionPane.showMessageDialog(null, "You Are " + age + " Years Old");
+		
+		double height =  Double.parseDouble(JOptionPane.showInputDialog("Please Enter Your Age : ")) ; 
+		JOptionPane.showMessageDialog(null, "You Are " + height + " Cm")	; 
+		
+		
+	}
+}
---
+++ userInput.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class userInput {
+	
+	public static void main(String[] args) {
+	
+		Scanner scanner  = new Scanner(System.in); 
+		
+		System.out.print("Please Enter Your Name : ");
+		String name = scanner.nextLine() ; 
+		 
+		
+		System.out.print("Please Enter Your Age : ");
+		int age = scanner.nextInt() ; 
+		
+		System.out.print("What's Your Favroite Food : ");
+		scanner.nextLine() ;
+		String favFood = scanner.nextLine() ; 
+		
+		
+		
+		
+		System.out.print("Hello " + name + " You Are " + age + " Years Old , And Your Favorite Food Is " + favFood);
+		
+		
+	}
+	
+	
+	
+}
